57af6ea2870744326cbc08207f60b477c42b2f2c
Jorgeflowers18 <jefo20156@gmail.com>
2019-11-05 21:57:30 -0500

    Clase 06 Manejo de archivos en python con CSV

--- ejercicio7.py
+++ ejercicio7.py
@@ -14,14 +14,8 @@ with open('data/trabajadores.csv') as midata:
     # con una longiutd de mas de 10 caracteres con un filter y list para que
     # sea visible
     print(list(filter(lambda x: len(list(x.items())[2][1]) > 10, data)))
-    
+    print("\n")
     # PREGUNTA DOS
-    # copiamos la lista de la primera pregunta
-    lista1 = list(filter(lambda x: len(list(x.items())[2][1]) > 10, data))
-    # separamos por posiciones las fechas para ser ordenadas
-    f = lambda x: list(x.items())[1][1].split("-")
-    # creamos una lista con las cuales se pueda ordenar de acuerdo al día de nacimiento 
-    lista2 = list(map(f, lista1))
-    # imprimimos la lista ordenada de acuerdo al dia con un sorted y un key
-    print(list(sorted(lista2, key = lambda x: x[2])))
+    # imprimimos la lista ordenada de acuerdo al dia de nacimiento con un sorted y un key
+    print(sorted(list(filter(lambda x: len(list(x.items())[2][1]) > 10, data)), key = lambda x: list(x.items())[1][1].split("-")[2]))
     
