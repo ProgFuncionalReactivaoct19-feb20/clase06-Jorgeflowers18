6b75f283e483a2c2ada299fd4b0c8077742b6554
Jorgeflowers18 <jefo20156@gmail.com>
2019-11-05 21:35:53 -0500

    Clase 06 Manejo de archivos con CSV

--- ejercicio7.py
+++ ejercicio7.py
@@ -9,11 +9,19 @@ def lineasDiccionario(archivo):
     
 with open('data/trabajadores.csv') as midata:
     data = list(lineasDiccionario(midata))
-    
-    # Primera pregunta
+    # PREGUNTA UNO
     # se declara una funcion con la cual se seleccione los que tienen paises 
     # con una longiutd de mas de 10 caracteres con un filter y list para que
     # sea visible
-    f = lambda x: len(list(x.items())[2][1]) > 10
-    print(list(filter(f, data)))
-   
+    print(list(filter(lambda x: len(list(x.items())[2][1]) > 10, data)))
+    
+    # PREGUNTA DOS
+    # copiamos la lista de la primera pregunta
+    lista1 = list(filter(lambda x: len(list(x.items())[2][1]) > 10, data))
+    # separamos por posiciones las fechas para ser ordenadas
+    f = lambda x: list(x.items())[1][1].split("-")
+    # creamos una lista con las cuales se pueda ordenar de acuerdo al día de nacimiento 
+    lista2 = list(map(f, lista1))
+    # imprimimos la lista ordenada de acuerdo al dia con un sorted y un key
+    print(list(sorted(lista2, key = lambda x: x[2])))
+    
